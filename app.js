d3a84f03641f81debda97c6d42ad09a26a1bad46
Abanob Essam <abanobessam19@gmail.com>
2025-06-27 15:53:09 +0300

    Establishing connection to DB

--- app.js
+++ app.js
@@ -4,11 +4,27 @@ const app = express();
 
 const path = require('path');
 
+app.use(express.json()); 
+
 app.use(express.static('public'));
+
+require("dotenv").config();
+
+require("./mongodb");
+
+const tickets = require("./models/ticket");
  
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public','index.html'));
 });
+
+app.post('/ticket/add', async (req, res) => {
+  const {name, phone, seatNum} = req.body;
+  let ticket = new tickets({name, phone, seatNum});
+  ticket = await ticket.save();
+  console.log(ticket);
+  return res.status(200).json({ticket: ticket});
+})
  
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
---
+++ models/ticket.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const Joi = require('joi');
+
+const Tickets = mongoose.model('Tickets', {
+    name: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    seatNum: {
+        type: String,
+        required: true
+    }
+})
+
+function handleTicketValidation(ticket) {
+    const schema = Joi.object({
+        name: Joi.string().min(3).max(120).required(),
+        phone:Joi.string().min(11).max(11).required(),
+        seatNum: Joi.string().min(2).max(20).required(),
+    });
+    return schema.validate(ticket, { abortEarly: false });
+}
+
+module.exports = Tickets
+module.exports.handleTicketValidation = handleTicketValidation
